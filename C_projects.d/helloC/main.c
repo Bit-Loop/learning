e5357cdecff2566dab9f88b156b11af2fa5e12b1
Isaiah Tyler Jackson <tyler@mail.byteloop.com>
2021-08-17 01:51:29 -0500

    Initial commit
     Changes to be committed:
    	new file:   C_projects.d/helloC/.idea/.gitignore
    	new file:   C_projects.d/helloC/.idea/.name
    	new file:   C_projects.d/helloC/.idea/misc.xml
    	new file:   C_projects.d/helloC/.idea/modules.xml
    	new file:   C_projects.d/helloC/.idea/untitled.iml
    	new file:   C_projects.d/helloC/.idea/vcs.xml
    	new file:   C_projects.d/helloC/CMakeCache.txt
    	new file:   C_projects.d/helloC/CMakeFiles/3.21.1/CMakeCCompiler.cmake
    	new file:   C_projects.d/helloC/CMakeFiles/3.21.1/CMakeDetermineCompilerABI_C.bin
    	new file:   C_projects.d/helloC/CMakeFiles/3.21.1/CMakeSystem.cmake
    	new file:   C_projects.d/helloC/CMakeFiles/3.21.1/CompilerIdC/CMakeCCompilerId.c
    	new file:   C_projects.d/helloC/CMakeFiles/3.21.1/CompilerIdC/a.out
    	new file:   C_projects.d/helloC/CMakeFiles/CMakeDirectoryInformation.cmake
    	new file:   C_projects.d/helloC/CMakeFiles/CMakeOutput.log
    	new file:   C_projects.d/helloC/CMakeFiles/Makefile.cmake
    	new file:   C_projects.d/helloC/CMakeFiles/Makefile2
    	new file:   C_projects.d/helloC/CMakeFiles/TargetDirectories.txt
    	new file:   C_projects.d/helloC/CMakeFiles/clion-environment.txt
    	new file:   C_projects.d/helloC/CMakeFiles/cmake.check_cache
    	new file:   C_projects.d/helloC/CMakeFiles/progress.marks
    	new file:   C_projects.d/helloC/CMakeFiles/runme.exe.dir/C.includecache
    	new file:   C_projects.d/helloC/CMakeFiles/runme.exe.dir/DependInfo.cmake
    	new file:   C_projects.d/helloC/CMakeFiles/runme.exe.dir/build.make
    	new file:   C_projects.d/helloC/CMakeFiles/runme.exe.dir/cmake_clean.cmake
    	new file:   C_projects.d/helloC/CMakeFiles/runme.exe.dir/compiler_depend.internal
    	new file:   C_projects.d/helloC/CMakeFiles/runme.exe.dir/compiler_depend.make
    	new file:   C_projects.d/helloC/CMakeFiles/runme.exe.dir/compiler_depend.ts
    	new file:   C_projects.d/helloC/CMakeFiles/runme.exe.dir/depend.internal
    	new file:   C_projects.d/helloC/CMakeFiles/runme.exe.dir/depend.make
    	new file:   C_projects.d/helloC/CMakeFiles/runme.exe.dir/flags.make
    	new file:   C_projects.d/helloC/CMakeFiles/runme.exe.dir/link.txt
    	new file:   C_projects.d/helloC/CMakeFiles/runme.exe.dir/main.c.o
    	new file:   C_projects.d/helloC/CMakeFiles/runme.exe.dir/main.c.o.d
    	new file:   C_projects.d/helloC/CMakeFiles/runme.exe.dir/progress.make
    	new file:   C_projects.d/helloC/CMakeFiles/untitled.dir/DependInfo.cmake
    	new file:   C_projects.d/helloC/CMakeFiles/untitled.dir/build.make
    	new file:   C_projects.d/helloC/CMakeFiles/untitled.dir/cmake_clean.cmake
    	new file:   C_projects.d/helloC/CMakeFiles/untitled.dir/compiler_depend.make
    	new file:   C_projects.d/helloC/CMakeFiles/untitled.dir/compiler_depend.ts
    	new file:   C_projects.d/helloC/CMakeFiles/untitled.dir/depend.make
    	new file:   C_projects.d/helloC/CMakeFiles/untitled.dir/flags.make
    	new file:   C_projects.d/helloC/CMakeFiles/untitled.dir/link.txt
    	new file:   C_projects.d/helloC/CMakeFiles/untitled.dir/main.c.o
    	new file:   C_projects.d/helloC/CMakeFiles/untitled.dir/main.c.o.d
    	new file:   C_projects.d/helloC/CMakeFiles/untitled.dir/progress.make
    	new file:   C_projects.d/helloC/CMakeLists.txt
    	new file:   C_projects.d/helloC/Makefile
    	new file:   C_projects.d/helloC/Testing/Temporary/LastTest.log
    	new file:   C_projects.d/helloC/cmake-build-debug/CMakeFiles/clion-log.txt
    	new file:   C_projects.d/helloC/cmake_install.cmake
    	new file:   C_projects.d/helloC/main.c
    	new file:   C_projects.d/helloC/runme.exe
    	new file:   C_projects.d/helloC/untitled
    	new file:   C_projects.d/helloC/untitled.cbp
    	new file:   C_projects.d/untitled/cmake-build-debug/CMakeFiles/clion-log.txt

---
+++ C_projects.d/helloC/main.c
@@ -0,0 +1,10 @@
+// Hello World by ITJ aug/17/20201
+#include <stdio.h>
+
+int main() {
+    printf("H E L L O !!\n"
+           "EXEs do not exist in Linux\n"
+           "EXEs are a lie!!!\n"
+           );
+    return 0;
+}
