3744709b203f0df1a1bbf1199ecbd088c4504e43
Isaiah Tyler Jackson <tyler@mail.byteloop.com>
2021-08-31 16:08:53 -0500

    Added scrach code for file IO to main.c and related files

--- compression/main.c
+++ compression/main.c
@@ -1,12 +1,34 @@
 /* ITJ - 08-31-21
  * Learning compression conecpts and FILE I/O the hardway.
- * 
+ * Huffman incoding by counting occurances of 0x00 to 0xFF and displaying the occurances
+ * in a histogram. 
  *
- * 
- * */
-#include <stdio.h> 
-
+ *  REF:
+ *     https://www.youtube.com/watch?v=_8-ht2AKyH4 iPointers and dynamic memory - stack vs heap
+ *
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
 int main(void){
 
+	// REF: http://www.dmulholl.com/lets-build/a-hexdump-utility.html
+	// REF: https://www.cplusplus.com/reference/cstdio/FILE/
+	// REF: 
+	FILE* file;
+	char* filePath = "./";
+
+	file = fopen("file", "r");
+        if (file == NULL) {
+            fprintf(stderr, "Error: cannot open the file '%s'.\n", filePath);
+            exit(1);
+	}
+	int maxBytes = 16;
+        uint8_t* buffer = (uint16_t*)malloc(maxBytes); 
+	int numBytes = fread(buffer, sizeof(uint16_t), maxBytes, file);
+	printf("%i", numBytes);
+	fclose(file);
 return 0;
 }
